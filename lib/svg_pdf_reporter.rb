229817687d94e9b3f148907742183a25eecc2b98
Eiji MATSUMOTO <e.mattsan@gmail.com>
2016-05-29 20:15:38 +0900

    generate pdf

--- lib/svg_pdf_reporter.rb
+++ lib/svg_pdf_reporter.rb
@@ -1,4 +1,22 @@
 module SvgPdfReporter
   class Base
+    include AbstractController::Rendering
+    include ActionView::Layouts
+
+    prepend_view_path('app/views')
+
+    def report(action)
+      to_svg(action)
+    end
+
+    def to_svg(action)
+      render_to_string layout: 'reporter', action: action, formats: :svg
+    end
+
+    private
+
+    def _prefixes
+      self.class.to_s.underscore.gsub(/_reporter/i, '')
+    end
   end
 end
