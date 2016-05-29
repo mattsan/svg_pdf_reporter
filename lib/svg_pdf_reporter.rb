f8bf91071d2d7de5fe6c5702ef20e7cf4ba80950
Eiji MATSUMOTO <e.mattsan@gmail.com>
2016-05-29 21:38:53 +0900

    add rendering pdf

--- lib/svg_pdf_reporter.rb
+++ lib/svg_pdf_reporter.rb
@@ -1,19 +1,37 @@
+require 'stringio'
+require 'rsvg2'
+
 module SvgPdfReporter
-  class Base
+  class Base < AbstractController::Base
     include AbstractController::Rendering
     include ActionView::Layouts
 
     prepend_view_path('app/views')
 
     def report(action)
-      to_svg(action)
+      source = generate_svg(action)
+      render_pdf(source)
     end
 
-    def to_svg(action)
+    def generate_svg(action)
       render_to_string layout: 'reporter', action: action, formats: :svg
     end
 
     private
+
+  def render_pdf(source)
+    sink = StringIO.new
+
+    handle = RSVG::Handle.new_from_data(source)
+    dim = handle.dimensions
+    Cairo::PDFSurface.new(sink, dim.width, dim.height) do |surface|
+      cairo = Cairo::Context.new(surface)
+      cairo.render_rsvg_handle(handle)
+    end
+    handle.close
+
+    sink.string
+  end
 
     def _prefixes
       self.class.to_s.underscore.gsub(/_reporter/i, '')
